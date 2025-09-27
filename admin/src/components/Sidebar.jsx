0a2dcbdd82bd6805893c34449269fee0291f0ac1
Hammad saeed <hammadsaeed6702@gmail.com>
2025-09-27 11:54:21 -0700

    creating add product page

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const Sidebar = () => {
     let navigate = useNavigate()
   return (
-    <div className='w-[18%] min-h-[100vh] border-r-[1px] py-[60px] fixed left-0 top-0'>
+    <div className=' w-[18%] min-h-[100vh] border-r-[1px] py-[60px] fixed left-0 top-0'>
       <div onClick={()=>navigate("/Add")} className='flex flex-col gap-4 pt-[40px] pl-[20%] text-[15px]'>
         <div className='flex items-center justify-center md:justify-start gap-3 border border-gray-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#2c7b89]'>
             <IoIosAddCircleOutline className='w-[20px] h-[20px]'/>
