84d7a7c52f1fe51a9dd3f0be39b06633359c1cfb
Hammad saeed <hammadsaeed6702@gmail.com>
2025-10-01 01:14:06 -0700

    create list product page

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { addProduct } from '../controllers/productController.js'
+import { addProduct, listProduct, removeProduct } from '../controllers/productController.js'
 import upload from "../middleware/multer.js"
+import adminAuth from "../middleware/adminAuth.js"
 
 let productRoutes = express.Router()
 
@@ -10,5 +11,8 @@ productRoutes.post("/addproduct",upload.fields([
     {name:"image3",maxCount:1},
     {name:"image4",maxCount:1},
 ]),addProduct)
+
+productRoutes.get('/list', listProduct)
+productRoutes.post('/remove/:id',adminAuth, removeProduct)
 
 export default productRoutes;
