0c82fcbefe70ec3df3465daa197500bd1ab04721
Hammad saeed <hammadsaeed6702@gmail.com>
2025-10-06 20:49:18 -0700

    create footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
 import Product from '../pages/Product'
+import OurPolicy from '../components/OurPolicy'
+import NewLetterBox from '../components/NewLetterBox'
 
 const Home = () => {
   return (
     <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] '>
       <Carousel/>
       <Product/>
+      <OurPolicy/>
+      <NewLetterBox/>
     </div>
   )
 }
